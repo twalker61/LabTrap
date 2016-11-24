1451567ab1232ab9f0a9451f24e33b211e4cc446
Phuc Huynh <phuchuynh@gatech.edu>
2016-11-23 20:10:27 -0500

    add the welcome scene and the navigation between the scenes

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,15 +2,20 @@ package game;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +23,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private Stage stage;
     private GamePane root;
     private GameScreen layer;
     private ScrollPane scroller;
@@ -79,8 +85,9 @@ public class Main extends Application {
             }
         });*/
 
+        stage = primaryStage;
         primaryStage.setTitle("Lab Trap!");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(welcomeScene(), 600, 600));
         primaryStage.show();
 
         root.getCanvas().requestFocus();
@@ -169,6 +176,22 @@ public class Main extends Application {
 
     private double clamp(double value, double min, double max) {
         return Math.min(max, Math.max(min, value));
+    }
+
+    public Parent welcomeScene() {
+        VBox vbox = new VBox(30);
+        Button startButton = new Button();
+        startButton.setText("START");
+        Button instructionButton = new Button();
+        instructionButton.setText("INSTRUCTION");
+        vbox.getChildren().addAll(startButton, instructionButton);
+        vbox.setAlignment(Pos.CENTER);
+        startButton.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent t){
+                stage.setScene(new Scene (root, 600, 600));
+            }
+        });
+        return vbox;
     }
 
 
